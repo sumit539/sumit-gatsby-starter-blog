5c2e615a7986ff093ddf17ee456bda02b53d4e05
sumit539 <sumitsngh539@gmail.com>
2020-03-21 02:19:39 +0530

    minor changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     description: `A starter blog demonstrating what Sumit is learning.`,
     siteUrl: `https://sumitblog.netlify.com/`,
     social: {
-      linkedin: `sumit539`,
+      linkedin: `in/sumit539`,
     },
   },
   plugins: [
